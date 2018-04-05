d8395234d7aa3dc23b2695180f27a07301120b55
hemanthkb <38094047+hemanthkb@users.noreply.github.com>
2018-04-05 14:19:11 +0530

    svn test

---
+++ New.java
@@ -0,0 +1,13 @@
+class A
+{
+public static void main(String args[])
+{
+System.out.println("welcome to my world");
+System.out.println("welcome to my singapore");
+System.out.println("welcome to my aus");
+System.out.println("welcome to my japan");
+System.out.println("welcome to my russia");
+System.out.println("welcome to my china");
+System.out.println("welcome to my usa");
+System.out.println("welcome to my india");
+}}
